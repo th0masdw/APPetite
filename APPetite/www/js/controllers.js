833dbbc4f67131f4ae4b1289f39d1346c91b86f2
alexandervandesteene <alexandervandesteene@gmail.com>
2015-05-06 09:42:39 +0200

    level switch werkt!

--- APPetite/www/js/controllers.js
+++ APPetite/www/js/controllers.js
@@ -2,9 +2,48 @@ var myApp = angular.module('starter.controllers', ['ngCordova', 'ionic' ]);
 
 myApp.controller('StartController', function($scope, $cordovaBarcodeScanner) {
 	$scope.scanBarcode = function() {
-		alert("geklikt");
         $cordovaBarcodeScanner.scan().then(function(imageData) {
             alert(imageData.text);
+            switch(imageData.text)
+            {
+                case "level1" : document.getElementById("button1").disabled=false;
+                                document.getElementById("button2").disabled=true;
+                                document.getElementById("button3").disabled=true;
+                                document.getElementById("button4").disabled=true;
+                                document.getElementById("button5").disabled=true; 
+                                break;
+                case "level2" : document.getElementById("button1").disabled=true;
+                                document.getElementById("button2").disabled=false;
+                                document.getElementById("button3").disabled=true;
+                                document.getElementById("button4").disabled=true;
+                                document.getElementById("button5").disabled=true; 
+                                break;
+                case "level3" : document.getElementById("button1").disabled=true;
+                                document.getElementById("button2").disabled=true;
+                                document.getElementById("button3").disabled=false;
+                                document.getElementById("button4").disabled=true;
+                                document.getElementById("button5").disabled=true; 
+                                break;
+                case "level4" : document.getElementById("button1").disabled=true;
+                                document.getElementById("button2").disabled=true;
+                                document.getElementById("button3").disabled=true;
+                                document.getElementById("button4").disabled=false;
+                                document.getElementById("button5").disabled=true; 
+                                break;
+                case "level5" : document.getElementById("button1").disabled=true;
+                                document.getElementById("button2").disabled=true;
+                                document.getElementById("button3").disabled=true;
+                                document.getElementById("button4").disabled=true;
+                                document.getElementById("button5").disabled=false; 
+                                break;
+                default : alert("you have scanned the wrong code");
+                          document.getElementById("button1").disabled=true;
+                          document.getElementById("button2").disabled=true;
+                          document.getElementById("button3").disabled=true;
+                          document.getElementById("button4").disabled=true;
+                          document.getElementById("button5").disabled=true; 
+                          break;
+            }
             console.log("Barcode Format -> " + imageData.format);
             console.log("Cancelled -> " + imageData.cancelled);
         }, function(error) {
